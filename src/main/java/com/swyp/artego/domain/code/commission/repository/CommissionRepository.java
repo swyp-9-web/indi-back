407a744b482ae9b62964e05c2a5cd3fabd6b66ba
ytjdud <102743371+ytjdud@users.noreply.github.com>
2025-04-23 23:31:11 +0900

    [Commission] refactor: 도메인 분리

--- src/main/java/com/swyp/artego/domain/code/commission/repository/CommissionRepository.java
+++ src/main/java/com/swyp/artego/domain/code/commission/repository/CommissionRepository.java
@@ -1,6 +1,6 @@
-package com.swyp.artego.domain.user.repository;
+package com.swyp.artego.domain.code.commission.repository;
 
-import com.swyp.artego.domain.user.entity.Commission;
+import com.swyp.artego.domain.code.commission.entity.Commission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommissionRepository extends JpaRepository<Commission, Long> {
