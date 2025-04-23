d86329c526351a0cd73bf7f95680582bac009734
ytjdud <102743371+ytjdud@users.noreply.github.com>
2025-04-23 17:55:13 +0900

    [All] feat: 엔티티 최초생성(0423기준)

---
+++ src/main/java/com/swyp/artego/domain/user/entity/ItemEmoji.java
@@ -0,0 +1,39 @@
+package com.swyp.artego.domain.user.entity;
+
+import com.swyp.artego.domain.post.entity.Item;
+import com.swyp.artego.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "item_emoji")
+public class ItemEmoji extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_emoji_id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(name = "emoji_type", nullable = false)
+    private String emojiType;
+
+    @Builder
+    public ItemEmoji(Item item, User user, String emojiType) {
+        this.item = item;
+        this.user = user;
+        this.emojiType = emojiType;
+    }
+}
