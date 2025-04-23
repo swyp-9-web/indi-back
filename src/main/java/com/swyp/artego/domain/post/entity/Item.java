c5a50e698683bbf92ed5ec2993683585cbfcf532
ytjdud <102743371+ytjdud@users.noreply.github.com>
2025-04-23 21:57:42 +0900

    feat: List<String>를 JSON으로 DB에 변환 저장해주는 컨버터 추가

--- src/main/java/com/swyp/artego/domain/post/entity/Item.java
+++ src/main/java/com/swyp/artego/domain/post/entity/Item.java
@@ -2,11 +2,14 @@ package com.swyp.artego.domain.post.entity;
 
 import com.swyp.artego.domain.user.entity.User;
 import com.swyp.artego.global.common.entity.BaseTimeEntity;
+import com.swyp.artego.global.converter.StringListConverter;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -29,8 +32,9 @@ public class Item extends BaseTimeEntity {
     @Column(name = "description", nullable = false)
     private String description;
 
+    @Convert(converter = StringListConverter.class)
     @Column(name= "img_url", nullable = false)
-    private String imgUrl;
+    private List<String> imgUrl;
 
     @Column(name = "price", nullable = false)
     private int price;
@@ -49,7 +53,7 @@ public class Item extends BaseTimeEntity {
     private String categoryType;
 
     @Builder
-    public Item(User user, String title, String description, String imgUrl, int price, String size, String material, String status, String categoryType) {
+    public Item(User user, String title, String description, List<String> imgUrl, int price, String size, String material, String status, String categoryType) {
         this.user = user;
         this.title = title;
         this.description = description;
