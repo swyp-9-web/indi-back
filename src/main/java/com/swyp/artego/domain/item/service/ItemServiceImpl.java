eada5e02cf73208fe5e678fe4285f207d3ba33ff
ytjdud <102743371+ytjdud@users.noreply.github.com>
2025-05-07 01:59:54 +0900

    [Item] refactor: 수정 API 에서 역할 단위로 메서드 분리

--- src/main/java/com/swyp/artego/domain/item/service/ItemServiceImpl.java
+++ src/main/java/com/swyp/artego/domain/item/service/ItemServiceImpl.java
@@ -56,6 +56,21 @@ public class ItemServiceImpl implements ItemService {
     }
 
     @Override
+    @Transactional(readOnly = true)
+    public List<ItemInfoResponse> getAllItems() {
+        return itemRepository.findAllByOrderByCreatedAtDesc()
+                .stream()
+                .map(ItemInfoResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ItemSearchResultResponse searchItems(AuthUser authUser, ItemSearchRequest request) {
+        return itemRepository.searchItems(authUser, request);
+    }
+
+    @Override
     @Transactional
     public ItemUpdateResponse updateItem(AuthUser authUser, Long itemId, ItemUpdateRequest request, List<MultipartFile> multipartFiles) {
         User user = userRepository.findByOauthId(authUser.getOauthId())
@@ -68,80 +83,18 @@ public class ItemServiceImpl implements ItemService {
             throw new BusinessExceptionHandler("작품을 수정할 권한이 없습니다.", ErrorCode.FORBIDDEN_ERROR);
         }
 
+        // 이미지 요소 검증 및 imgUrls 반환, 사용하지 않는 사진은 삭제
         List<String> orderedList = request.getOrderedImageList();
+        validateFileSizeAndNameMatch(multipartFiles, orderedList);
 
-        // 1. 새로 추가될 이미지 이름 리스트
-        List<String> expectedNewImageNames = orderedList.stream()
-                .filter(name -> !name.startsWith("https"))
-                .toList();
+        List<String> updatedImageUrls = uploadNewImagesInOrderWithRollback(multipartFiles, orderedList);
 
-        // 2. 파일 이름 리스트
-        List<String> actualUploadedFileNames = Optional.ofNullable(multipartFiles)
-                .orElse(Collections.emptyList())
-                .stream()
-                .map(MultipartFile::getOriginalFilename)
-                .toList();
-
-        // 3. 개수 비교
-        if (expectedNewImageNames.size() != actualUploadedFileNames.size()) {
-            throw new BusinessExceptionHandler("새 이미지 개수와 업로드된 파일 개수가 일치하지 않습니다.", ErrorCode.BAD_REQUEST_ERROR);
-
-        }
-
-        // 4. 파일 이름 일치 여부 확인 (순서 상관 있는 버전)
-        for (int i = 0; i < expectedNewImageNames.size(); i++) {
-            if (!expectedNewImageNames.get(i).equals(actualUploadedFileNames.get(i))) {
-                throw new BusinessExceptionHandler("파일 이름 불일치: orderedList에 있는 사진 이름은 " +
-                        expectedNewImageNames.get(i) + " 이지만, multipartfile에는 " + actualUploadedFileNames.get(i) + " 이 전송되었습니다.", ErrorCode.BAD_REQUEST_ERROR);
-            }
-        }
-
-        // 이미지 수정 처리 TODO: fileService 로 분리?
-        List<String> finalImageUrls = new ArrayList<>();
-        Iterator<MultipartFile> fileIterator = Optional.ofNullable(multipartFiles).orElse(Collections.emptyList()).iterator();
-        List<String> uploadedKeys = new ArrayList<>();
-
-        for (String imageRef : orderedList) {
-            if (imageRef.startsWith("https")) {
-                finalImageUrls.add(imageRef);
-            } else {
-                MultipartFile file = fileIterator.next();
-                FileUploadResponseExample response;
-                try {
-                    response = fileService.uploadFile(file, folderName);
-                    uploadedKeys.add(response.getUploadKey());
-                } catch (BusinessExceptionHandler e) {
-                    log.info("[ItemService] 수정 API: #### 업로드된 파일 롤백 시작 ####");
-                    fileService.deleteFiles(uploadedKeys);
-                    throw e; // 단일 업로드에서도 이미 비즈니스 예외로 변환했기 때문에 그대로 던짐
-                }
-                finalImageUrls.add(response.getUploadFileUrl());
-            }
-        }
-
-        List<String> originalUrls = item.getImgUrls(); // 기존 이미지 URL
-        List<String> urlsToKeep = finalImageUrls;     // 최종 유지할 이미지 URL
-
-        // 삭제할 URL만 추출
-        List<String> deletedUrls = originalUrls.stream()
-                .filter(url -> !urlsToKeep.contains(url))
-                .toList();
-
-        // URL -> key 변환 로직 (예: https://s3.com/folder/key.jpg → key.jpg)
-        List<String> deletedKeys = deletedUrls.stream()
-                .map(fileService::extractKeyFromImgUrl) // 또는 직접 key 추출 로직
-                .toList();
-
-        // 삭제 실행
-        if (!deletedKeys.isEmpty()) {
-            log.info("[ItemService] 수정 API: #### 게시글에서 내린 사진을 S3에서 실제로 삭제 ####");
-            fileService.deleteFiles(deletedKeys);
-        }
+        deleteRemovedImages(item.getImgUrls(), updatedImageUrls);
 
         // 이미지 외 나머지 요소 수정
         ItemCreateRequest.ItemSize requestSize = request.getSize();
         SizeType sizeType = calculateSizeType(requestSize.getWidth(), requestSize.getHeight(), requestSize.getDepth());
-        request.applyToEntity(item, finalImageUrls, sizeType);
+        request.applyToEntity(item, updatedImageUrls, sizeType);
 
         return ItemUpdateResponse.fromEntity(item);
     }
@@ -164,15 +117,6 @@ public class ItemServiceImpl implements ItemService {
         return ItemDeleteResponse.fromEntity(item);
     }
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<ItemInfoResponse> getAllItems() {
-        return itemRepository.findAllByOrderByCreatedAtDesc()
-                .stream()
-                .map(ItemInfoResponse::fromEntity)
-                .collect(Collectors.toList());
-    }
-
     /**
      * 제품의 가로, 세로, 높이를 토대로 제품의 사이즈(S,M,L 혹은 X)를 구한다.
      *
@@ -190,12 +134,91 @@ public class ItemServiceImpl implements ItemService {
         else return SizeType.L;
     }
 
+    /**
+     * [수정 API] 파일 이름 및 개수 유효성 검증
+     * <p>
+     * orderedList로 부터 새로 추가될 이미지 리스트와 multipartFiles로부터 새로 추가될 이미지의 실제 이름 리스트를 비교한다.
+     * 두 리스트의 사이즈를 비교하고 실제 이름을 비교한다.
+     *
+     * @param multipartFiles
+     * @param orderedList    사용자가 새로 정렬한 사진 순서
+     */
+    private static void validateFileSizeAndNameMatch(List<MultipartFile> multipartFiles, List<String> orderedList) {
 
-    @Override
-    @Transactional(readOnly = true)
-    public ItemSearchResultResponse searchItems(AuthUser authUser, ItemSearchRequest request) {
-        return itemRepository.searchItems(authUser, request);
+        List<String> expectedNewImageNames = orderedList.stream()
+                .filter(name -> !name.startsWith("https"))
+                .toList();
+
+        List<String> actualUploadedFileNames = Optional.ofNullable(multipartFiles)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(MultipartFile::getOriginalFilename)
+                .toList();
+
+        if (expectedNewImageNames.size() != actualUploadedFileNames.size()) {
+            throw new BusinessExceptionHandler("새 이미지 개수와 업로드된 파일 개수가 일치하지 않습니다.", ErrorCode.BAD_REQUEST_ERROR);
+
+        }
+
+        for (int i = 0; i < expectedNewImageNames.size(); i++) {
+            if (!expectedNewImageNames.get(i).equals(actualUploadedFileNames.get(i))) {
+                throw new BusinessExceptionHandler("파일 이름 불일치: orderedList에 있는 사진 이름은 " +
+                        expectedNewImageNames.get(i) + " 이지만, multipartfile에는 " + actualUploadedFileNames.get(i) + " 이 전송되었습니다.", ErrorCode.BAD_REQUEST_ERROR);
+            }
+        }
     }
 
+    /**
+     * [수정 API] 순서대로 새 이미지를 업로드하고 실패 시 롤백까지 수행한다.
+     *
+     * @param multipartFiles 새 이미지 정보
+     * @param orderedList    사용자가 원하는 이미지 순서
+     * @return DB에 업데이트할 새로운 imgUrls
+     */
+    private List<String> uploadNewImagesInOrderWithRollback(List<MultipartFile> multipartFiles, List<String> orderedList) {
+        List<String> finalImageUrls = new ArrayList<>();
+        Iterator<MultipartFile> fileIterator = Optional.ofNullable(multipartFiles).orElse(Collections.emptyList()).iterator();
+        List<String> uploadedKeys = new ArrayList<>();
 
+        for (String imageRef : orderedList) {
+            if (imageRef.startsWith("https")) {
+                finalImageUrls.add(imageRef);
+            } else {
+                MultipartFile file = fileIterator.next();
+                FileUploadResponseExample response;
+                try {
+                    response = fileService.uploadFile(file, folderName);
+                    uploadedKeys.add(response.getUploadKey());
+                } catch (BusinessExceptionHandler e) {
+                    log.info("[ItemService] 수정 API: #### 업로드된 파일 롤백 시작 ####");
+                    fileService.deleteFiles(uploadedKeys);
+                    throw e;
+                }
+                finalImageUrls.add(response.getUploadFileUrl());
+            }
+        }
+        return finalImageUrls;
+    }
+
+    /**
+     * [수정 API] 삭제할 이미지 추출, S3에서 삭제 실행
+     *
+     * @param originalUrls   기존 게시글의 이미지 URLs
+     * @param updatedImgUrls 수정한 결과 이미지 Urls
+     */
+    private void deleteRemovedImages(List<String> originalUrls, List<String> updatedImgUrls) {
+
+        List<String> deletedUrls = originalUrls.stream()
+                .filter(url -> !updatedImgUrls.contains(url))
+                .toList();
+
+        List<String> deletedKeys = deletedUrls.stream()
+                .map(fileService::extractKeyFromImgUrl)
+                .toList();
+
+        if (!deletedKeys.isEmpty()) {
+            log.info("[ItemService] 수정 API: #### 게시글에서 내린 사진을 S3에서 삭제 ####");
+            fileService.deleteFiles(deletedKeys);
+        }
+    }
 }
