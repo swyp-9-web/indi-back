86c6221138d217dc6718e9e31e7762a0169599fd
ytjdud <102743371+ytjdud@users.noreply.github.com>
2025-05-06 23:44:30 +0900

    [Item] feat: 수정 API 기본 구현

--- src/main/java/com/swyp/artego/domain/item/service/ItemService.java
+++ src/main/java/com/swyp/artego/domain/item/service/ItemService.java
@@ -2,12 +2,11 @@ package com.swyp.artego.domain.item.service;
 
 import com.swyp.artego.domain.item.dto.request.ItemCreateRequest;
 import com.swyp.artego.domain.item.dto.request.ItemSearchRequest;
-import com.swyp.artego.domain.item.dto.response.ItemCreateResponse;
-import com.swyp.artego.domain.item.dto.response.ItemDeleteResponse;
-import com.swyp.artego.domain.item.dto.response.ItemInfoResponse;
-import com.swyp.artego.domain.item.dto.response.ItemSearchResultResponse;
+import com.swyp.artego.domain.item.dto.request.ItemUpdateRequest;
+import com.swyp.artego.domain.item.dto.response.*;
 import com.swyp.artego.global.auth.oauth.model.AuthUser;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface ItemService {
@@ -15,16 +14,26 @@ public interface ItemService {
     /**
      * 작품을 생성한다.
      *
-     * @param user
+     * @param authUser
      * @param request
      * @param multipartFiles
      * @param folderName
      * @return
      */
-    ItemCreateResponse createItem(AuthUser user, ItemCreateRequest request, List<MultipartFile> multipartFiles, String folderName);
+    ItemCreateResponse createItem(AuthUser authUser, ItemCreateRequest request, List<MultipartFile> multipartFiles, String folderName);
 
     /**
-     * 작품 삭제
+     * 작품을 수정한다.
+     *
+     * @param authUser 수정을 시도하는 사용자
+     * @param itemId   수정하려는 작품의 id
+     * @param request  수정 내용
+     * @return ItemUpdateResponse
+     */
+    ItemUpdateResponse updateItem(AuthUser authUser, Long itemId, ItemUpdateRequest request, List<MultipartFile> multipartFiles, String folderName);
+
+    /**
+     * 작품을 삭제한다.
      * 물리적 삭제가 아닌 statusType을 HIDE로 변경한다.(논리적 삭제)
      *
      * @param itemId 삭제하려는 작품의 id
