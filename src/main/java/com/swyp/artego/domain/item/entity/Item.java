9687091c33a58bf9b7b266c7fa1b2d2c27be1289
ytjdud <102743371+ytjdud@users.noreply.github.com>
2025-05-15 14:44:48 +0900

    [Item] fix: 한글 인코딩을 고려하여 작품 설명 글자 수 제한을 1000byte 까지 허용
    - 공백 제외 한글 400자, 일반적인 글쓰기 기준

--- src/main/java/com/swyp/artego/domain/item/entity/Item.java
+++ src/main/java/com/swyp/artego/domain/item/entity/Item.java
@@ -31,7 +31,7 @@ public class Item extends BaseTimeEntity {
     private String title;
 
     @Setter
-    @Column(name = "description", nullable = false, length = 810) // 공백 제외 400자 제한이므로 *2 +@
+    @Column(name = "description", nullable = false, length = 1000) // 일반적인 글쓰기 기준, 공백제외 한글 400자는 약 890byte
     private String description;
 
     @Setter
