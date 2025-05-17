47e3c31a49733e6e4159124cd4dbe4488acf98d1
dudtlr <wjsdudtlr86@naver.com>
2025-05-17 23:32:24 +0900

    fix: 실시간 반영

--- src/main/java/com/swyp/artego/domain/item/repository/ItemRepository.java
+++ src/main/java/com/swyp/artego/domain/item/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package com.swyp.artego.domain.item.repository;
 import com.swyp.artego.domain.item.entity.Item;
 import com.swyp.artego.domain.item.enums.StatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,6 +19,28 @@ public interface ItemRepository extends JpaRepository<Item, Long> , ItemSearchRe
     List<Item> findAllByOrderByCreatedAtDesc();
 
     List<Item> findByStatusType(StatusType statusType);
+
+    @Modifying
+    @Query("UPDATE Item i SET i.scrapCount = i.scrapCount + :delta WHERE i.id = :itemId")
+    void incrementScrapCount(@Param("itemId") Long itemId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE Item i SET i.totalReactionCount = i.totalReactionCount + :delta WHERE i.id = :itemId")
+    void incrementReactionCount(@Param("itemId") Long itemId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE Item i SET i.likeCount = i.likeCount + :delta WHERE i.id = :itemId")
+    void incrementLikeCount(@Param("itemId") Long itemId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE Item i SET i.wantCount = i.wantCount + :delta WHERE i.id = :itemId")
+    void incrementWantCount(@Param("itemId") Long itemId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE Item i SET i.revisitCount = i.revisitCount + :delta WHERE i.id = :itemId")
+    void incrementRevisitCount(@Param("itemId") Long itemId, @Param("delta") int delta);
+
+
 }
 
 
--- src/main/java/com/swyp/artego/domain/user/repository/UserRepository.java
+++ src/main/java/com/swyp/artego/domain/user/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.swyp.artego.domain.user.repository;
 
 import com.swyp.artego.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +12,22 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByOauthId(String oauthId);
     boolean existsByNickname(String nickname);
+
+
+    @Modifying
+    @Query("UPDATE User u SET u.scrapCount = u.scrapCount + :delta WHERE u.id = :userId")
+    void incrementUserScrapCount(@Param("userId") Long userId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE User u SET u.reactionCount = u.reactionCount + :delta WHERE u.id = :userId")
+    void incrementUserReactionCount(@Param("userId") Long userId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE User u SET u.followerCount = u.followerCount + :delta WHERE u.id = :userId")
+    void incrementFollowerCount(@Param("userId") Long userId, @Param("delta") int delta);
+
+    @Modifying
+    @Query("UPDATE User u SET u.itemCount = u.itemCount + :delta WHERE u.id = :userId")
+    void incrementItemCount(@Param("userId") Long userId, @Param("delta") int delta);
+
 }
