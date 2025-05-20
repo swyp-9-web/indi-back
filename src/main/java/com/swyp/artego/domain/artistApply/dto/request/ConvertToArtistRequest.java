665f8d73870c29a7e34e42ede70aab00cf059eef
dudtlr <wjsdudtlr86@naver.com>
2025-05-21 01:33:53 +0900

    [ArtistApply] feat : 작가 권한 부여 API 추가

---
+++ src/main/java/com/swyp/artego/domain/artistApply/dto/request/ConvertToArtistRequest.java
@@ -0,0 +1,11 @@
+package com.swyp.artego.domain.artistApply.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ConvertToArtistRequest {
+
+    @NotNull(message = "user_id는 필수입니다.")
+    private Long userId;
+}
--- src/main/java/com/swyp/artego/domain/artistApply/service/ArtistApplyService.java
+++ src/main/java/com/swyp/artego/domain/artistApply/service/ArtistApplyService.java
@@ -1,6 +1,7 @@
 package com.swyp.artego.domain.artistApply.service;
 
 import com.swyp.artego.domain.artistApply.dto.request.ArtistApplyCreateRequest;
+import com.swyp.artego.domain.artistApply.dto.request.ConvertToArtistRequest;
 import com.swyp.artego.domain.artistApply.dto.response.ArtistApplyCreateResponse;
 import com.swyp.artego.global.auth.oauth.model.AuthUser;
 
@@ -11,6 +12,13 @@ public interface ArtistApplyService {
      */
 
     public ArtistApplyCreateResponse createArtistApply(AuthUser authUser, ArtistApplyCreateRequest request);
+
+
+    /**
+     * 유저 -> 작가 전환 API
+     */
+
+    void convertUserToArtist(AuthUser adminUser, ConvertToArtistRequest request);
 }
 
 
